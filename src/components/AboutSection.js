b996edebeaa993cebcbf7f4fb961c1557bc31d01
Neesh <avaneeshsamsi@gmail.com>
2021-07-30 16:44:04 +0530

    12. Framer Motion

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -2,26 +2,28 @@ import React from "react";
 import home1 from "../img/home1.png";
 
 // Import Styled Components
-import styled from "styled-components";
 import { StlAbout, StlDescription, StlImage, StlHide } from "../styles";
+
+// Import Framer Motion
+import { motion } from "framer-motion";
 
 const AboutSection = () => {
   return (
     <StlAbout>
       <StlDescription>
-        <div className="title">
+        <motion.div>
           <StlHide>
-            <h2>We work to make</h2>
+            <motion.h2>We work to make</motion.h2>
           </StlHide>
           <StlHide>
-            <h2>
+            <motion.h2>
               your <span>dreams</span>
-            </h2>
+            </motion.h2>
           </StlHide>
           <StlHide>
-            <h2>come true.</h2>
+            <motion.h2>come true.</motion.h2>
           </StlHide>
-        </div>
+        </motion.div>
         <p>
           Contact us for an photography or videography ideas that you have. We
           have professionals with amazing skills.
