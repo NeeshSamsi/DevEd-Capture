b5c5d604b30a68387791482e7ab13d56310e8e2c
Neesh <avaneeshsamsi@gmail.com>
2021-08-01 16:54:19 +0530

    24. Nav Underline Animation - CAPTURE COMPLETE

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,8 +1,11 @@
 import React from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+import { useLocation } from "react-router-dom";
 
 const Nav = () => {
+  const { pathname } = useLocation();
   return (
     <StlNav>
       <h1>
@@ -14,12 +17,27 @@ const Nav = () => {
       <ul>
         <li>
           <Link to="/">1. About Us</Link>
+          <StlUnderline
+            transition={{ duration: 0.75 }}
+            initial={{ width: "0%" }}
+            animate={{ width: pathname === "/" ? "50%" : "0%" }}
+          />
         </li>
         <li>
           <Link to="/work">2. Our Work</Link>
+          <StlUnderline
+            transition={{ duration: 0.75 }}
+            initial={{ width: "0%" }}
+            animate={{ width: pathname === "/work" ? "50%" : "0%" }}
+          />
         </li>
         <li>
           <Link to="/contact">3. Contact Us</Link>
+          <StlUnderline
+            transition={{ duration: 0.75 }}
+            initial={{ width: "0%" }}
+            animate={{ width: pathname === "/contact" ? "50%" : "0%" }}
+          />
         </li>
       </ul>
     </StlNav>
@@ -75,6 +93,19 @@ const StlNav = styled.nav`
     li {
       padding: 0;
     }
+  }
+`;
+
+const StlUnderline = styled(motion.div)`
+  height: 0.3rem;
+  width: 0;
+  background: #23d997;
+  position: absolute;
+  bottom: -80%;
+  left: 60%;
+
+  @media (max-width: 1300px) {
+    left: 0%;
   }
 `;
 
