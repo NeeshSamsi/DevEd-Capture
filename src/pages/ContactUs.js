42a4cb7c4fcce726e19a7ed2abcabd4a5c00db02
Neesh <avaneeshsamsi@gmail.com>
2021-08-01 13:36:48 +0530

    21. Contact Page and Animations

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -3,19 +3,79 @@ import React from "react";
 // Animation
 import { motion } from "framer-motion";
 import { pageAnimation } from "../animation";
+import { titleAnimation } from "../animation";
+
+// Import Styled
+import styled from "styled-components";
 
 const ContactUs = () => {
   return (
-    <motion.div
+    <StlContact
       variants={pageAnimation}
       initial="hidden"
       animate="show"
       exit="exit"
       style={{ background: "#fff" }}
     >
-      <h1>Contact Us</h1>
-    </motion.div>
+      <StlTitle>
+        <StlHide>
+          <motion.h2 variants={titleAnimation}>Get in touch.</motion.h2>
+        </StlHide>
+      </StlTitle>
+      <div>
+        <StlHide>
+          <StlSocial variants={titleAnimation}>
+            <StlCircle />
+            <h2>Send us a message.</h2>
+          </StlSocial>
+        </StlHide>
+        <StlHide>
+          <StlSocial variants={titleAnimation}>
+            <StlCircle />
+            <h2>Send an email.</h2>
+          </StlSocial>
+        </StlHide>
+        <StlHide>
+          <StlSocial variants={titleAnimation}>
+            <StlCircle />
+            <h2>Social Media.</h2>
+          </StlSocial>
+        </StlHide>
+      </div>
+    </StlContact>
   );
 };
+
+// Styled Components
+const StlContact = styled(motion.div)`
+  padding: 5rem 10rem;
+  color: #353535;
+  min-height: 90vh;
+`;
+
+const StlTitle = styled.div`
+  margin: 4rem;
+  color: black;
+`;
+
+const StlHide = styled.div`
+  overflow: hidden;
+`;
+
+const StlSocial = styled(motion.div)`
+  display: flex;
+  align-items: center;
+
+  h2 {
+    margin: 2rem;
+  }
+`;
+
+const StlCircle = styled.div`
+  border-radius: 50%;
+  width: 3rem;
+  height: 3rem;
+  background: #353535;
+`;
 
 export default ContactUs;
